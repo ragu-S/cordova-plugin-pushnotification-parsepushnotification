97f21b3e9793624c72369ce5ea47d7f3e28c820b
Ragu Sivanandha <ragu.sivanandha@gmail.com>
2016-01-27 03:52:31 -0500

    Update ParseBroadcastReceiver.java

--- src/android/ParseBroadcastReceiver.java
+++ src/android/ParseBroadcastReceiver.java
@@ -14,17 +14,7 @@ import org.json.JSONObject;
 
 public class ParseBroadcastReceiver extends ParsePushBroadcastReceiver {
     private static final String LOG_TAG = "ParsePushNotificationPlugin";
-    private static ParsePushNotificationPlugin pluginInstance = null;
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        if(ParsePushNotificationPlugin.callbackContextKeepCallback == null) {
-            SharedPreferences sharedPref = context.getApplicationContext().getSharedPreferences("cordova-plugin-pushnotification-parse", Context.MODE_PRIVATE);
-            String applicationId = sharedPref.getString("applicationId", "");
-            String clientKey = sharedPref.getString("clientKey", "");
-            Parse.initialize(context.getApplicationContext(), applicationId, clientKey);
-        }
-        super.onReceive(context, intent);
-    }
+
     @Override
     protected void onPushReceive(Context context, Intent intent) {
         Log.d(LOG_TAG, String.format("%s", "notificationreceived!"));
@@ -39,21 +29,27 @@ public class ParseBroadcastReceiver extends ParsePushBroadcastReceiver {
         }
 
         super.onPushReceive(context, intent);
+
     }
     @Override
     protected void onPushOpen(Context context, Intent intent) {
         Log.d(LOG_TAG, String.format("%s", "notification opened!"));
 
         try {
-
             JSONObject extras = new JSONObject(intent.getStringExtra("com.parse.Data"));
             extras.put("notificationReceived", "true");
             ParsePushNotificationPlugin.receivedNotification = extras;
+
+            if(ParsePushNotificationPlugin.state == ParsePushNotificationPlugin.AppState.BACKGROUND || ParsePushNotificationPlugin.state == ParsePushNotificationPlugin.AppState.DESTROYED) {
+                super.onPushOpen(context, intent);
+            }
+            else if(ParsePushNotificationPlugin.state == ParsePushNotificationPlugin.AppState.FOREGROUND) {
+                // Do JS callback in app
+                ParsePushNotificationPlugin.selfReference.notificationReceivedCB();
+            }
         }
         catch (JSONException e) {
             Log.d(LOG_TAG, String.format("%s", "JSON error!"));
         }
-
-        super.onPushOpen(context, intent);
     }
 }
